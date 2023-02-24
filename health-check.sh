5e5e654705e01a88db6532123a9ace96665e09b2
Jinwen Guo <jinwen@plus.ai>
2023-02-24 21:29:56 +0800

    update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -58,9 +58,9 @@ done
 
 if [[ $commit == true ]]
 then
-  # Let's make Vijaye the most productive person on GitHub.
-  git config --global user.name 'Vijaye Raji'
-  git config --global user.email 'vijaye@statsig.com'
+  # Let's make Jinwen the most productive person on GitHub.
+  git config --global user.name 'Jinwen Guo'
+  git config --global user.email 'blackglassesguo@gmail.com'
   git add -A --force logs/
   git commit -am '[Automated] Update Health Check Logs'
   git push
